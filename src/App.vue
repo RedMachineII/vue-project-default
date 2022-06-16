6a2c94b92c8c89b73f8f3bbedc4bad64373e8bd5
RedMachineII <kiattisak.mai@cpf.co.th>
2022-06-16 14:25:03 +0700

    v1

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,38 @@
 <template>
   <div id="app">
-    <nav>
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </nav>
-    <router-view/>
+    <b-navbar type="dark" variant="dark" fixed="top">
+      <b-navbar-nav>
+        <b-nav-item href="#">Home</b-nav-item>
+
+        <!-- Navbar dropdowns -->
+        <b-nav-item-dropdown text="Lang" right>
+          <b-dropdown-item><router-link to="/">Home</router-link></b-dropdown-item>
+          <b-dropdown-item><router-link to="/about">About</router-link></b-dropdown-item>
+        </b-nav-item-dropdown>
+
+        <b-nav-item-dropdown text="User" right>
+          <b-dropdown-item href="#">Account</b-dropdown-item>
+          <b-dropdown-item href="#">Settings</b-dropdown-item>
+        </b-nav-item-dropdown>
+      </b-navbar-nav>
+    </b-navbar>
+    <router-view style="padding-top: 60px; padding-bottom: 60px" />
+    <b-navbar type="dark" variant="dark" fixed="bottom">
+      <b-navbar-nav>
+        <b-nav-item href="#">Home</b-nav-item>
+
+        <!-- Navbar dropdowns -->
+        <b-nav-item-dropdown text="Lang" right>
+          <b-dropdown-item><router-link to="/">Home</router-link></b-dropdown-item>
+          <b-dropdown-item><router-link to="/about">About</router-link></b-dropdown-item>
+        </b-nav-item-dropdown>
+
+        <b-nav-item-dropdown text="User" right>
+          <b-dropdown-item href="#">Account</b-dropdown-item>
+          <b-dropdown-item href="#">Settings</b-dropdown-item>
+        </b-nav-item-dropdown>
+      </b-navbar-nav>
+    </b-navbar>
   </div>
 </template>
 
